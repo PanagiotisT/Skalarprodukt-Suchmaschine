178d6b6cb42a018ec88f6a8ef5ab7727253b2fd1
PanagiotisT <Panagiotis.tsitsos@live.de>
2019-06-15 20:32:44 +0200

    searchquery

--- Suchmaschine/src/app/app.module.ts
+++ Suchmaschine/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KopfbereichComponent } from './kopfbereich/kopfbereich.component';
 import { HauptteilComponent } from './hauptteil/hauptteil.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HauptteilComponent } from './hauptteil/hauptteil.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Suchmaschine/src/app/hauptteil/hauptteil.component.ts
+++ Suchmaschine/src/app/hauptteil/hauptteil.component.ts
@@ -8,14 +8,60 @@ import { Vector } from '../vector';
 })
 export class HauptteilComponent implements OnInit {
 
+  // Searchquery
+  searchquery: string;
+  cleanSearchquery: string;
+  symbole: string[];
+
   // Später Vektor Suchanfrage und Vektor SeitenInhalte
   vektor1: Vector
   vektor2: Vector
 
-
   constructor() { }
 
   ngOnInit() {
+  }
+
+  searchqueryUpdate() {
+    // Suchquery in einzelne Wörter aufteilen diese dann in einen Vektor umwandeln 
+    // Später noch filter funktion hinzufügen, dass füllwörter weggelassen werden etc.
+    // Oder mit gewichtungen arbeiten dass bestimmte Wörter einen höheren Wert haben.
+    
+
+    // Überprüfen ob searchquery symbole enthält z.b ?!,:; etc.. Falls ja Symbole entfernen
+    let symbole = ['?' , '!' , ',' , ';']
+
+
+    // Bug mit den Leerzeichen fixxen 
+    //  -----------------------------------------------------------------------------
+    console.log(`Vor der Überprüfung:  ${this.searchquery}`)
+
+    this.cleanSearchquery = this.containsSymbol(this.searchquery, symbole)
+
+    console.log(`Nach der Überprüfung:  ${this.cleanSearchquery}`)
+
+    let woerter = this.cleanSearchquery.split(" ")
+
+    console.log(`Wörter gesplittet: ${woerter}. Es gibt ${woerter.length} Wörter.`)
+    // ---------------------------------------------------------------------------------------
+
+  }
+
+  containsSymbol(target: string, pattern: string[]) {
+
+    // Führe die Funktion aus, wenn was im target string steht
+    if(target != undefined){
+      
+      pattern.forEach( (element) => {
+        // Wenn eins der Symbole ethalten ist entferne es
+        if(target.includes(element)){
+          target = target.replace(element, '')
+          target = target.replace(' ', '')
+        }           
+      });
+
+      return target
+    }
   }
 
 }
